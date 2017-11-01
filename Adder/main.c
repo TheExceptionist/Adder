54aab62ab2121957f90e712366f5a445b2a23e7a
Kenny Stepney <kennystepney@hotmail.com>
2017-11-01 10:06:19 -0500

    Initial Commit

---
+++ Adder/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Adder
+//
+//  Created by Kenneth Stepney on 11/1/17.
+//  Copyright © 2017 Kenneth Stepney. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
